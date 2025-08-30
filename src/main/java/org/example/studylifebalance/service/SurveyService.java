e23d34b0741f6c8e7050e155f5307e182a684b87
SeoyeonH <stellastar0921@gmail.com>
2025-08-31 04:51:06 +0900

    Manual edits to Category.java and SurveyService.java before merge

--- src/main/java/org/example/studylifebalance/service/SurveyService.java
+++ src/main/java/org/example/studylifebalance/service/SurveyService.java
@@ -1,6 +1,5 @@
-package org.example.studylifebalance.service;
+package main.java.org.example.studylifebalance.service;
 
-import org.example.studylifebalance.MBTI;
 import org.example.studylifebalance.repository.CategoryRepository;
 import org.example.studylifebalance.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,7 @@ public class SurveyService {
 
     // 전체 category 비율
     public int getCategoryPercentage(String categoryCode) {
-        int categoryCount = userRepository.countByCategory_CategoryId(categoryCode);
+        int categoryCount = userRepository.countByCategoryId(categoryCode);
         long totalCount = userRepository.count();
         if (totalCount == 0) return 0;
         return Math.round((float) categoryCount / totalCount * 100);
@@ -26,7 +25,7 @@ public class SurveyService {
 
     // 학교 내 category 비율
     public int getCategoryPercentageInCollege(String categoryCode, String college) {
-        int categoryCount = userRepository.countByCategory_CategoryIdAndCollege(categoryCode, college);
+        int categoryCount = userRepository.countByCategoryIdAndCollege(categoryCode, college);
         int totalCount = userRepository.countByCollege(college);
         if (totalCount == 0) return 0;
         return Math.round((float) categoryCount / totalCount * 100);
@@ -62,80 +61,78 @@ public class SurveyService {
         return Math.round((double) restTime / totalTime * 100);
     }
 
-    public double getOutsideRatio(boolean outsideTime, String mbti) {
-        MBTI mbtiEnum = MBTI.valueOf(mbti.toUpperCase());
-
-        // 점수 가져오기
-        double mbtiScore = mbtiEnum.getScore();
-        int outsideTimeScore = outsideTime ? 1 : 0;
-        double score = (mbtiScore + outsideTimeScore) / 3;
-        double roundedScore = Math.round(score * 100.0) / 100.0;
-        return roundedScore;
-    }
-
     //카테고리 계산
     public Pair<String, String> getCategory(String major, double majorRatio, double studyTimeRatio, double outsideRatio) {
         String resultCode = "";
 
-        // 계열
-        switch (major) {
-            case "인문사회계열":
-                resultCode += "H";
-                break;
-            case "자연계열":
-                resultCode += "N";
-                break;
-            case "공학계열":
-                resultCode += "E";
-                break;
-            case "예술계열":
-                resultCode += "A";
-                break;
-            case "체육계열":
-                resultCode += "P";
-                break;
+        // // 계열
+        // switch (major) {
+        //     case "인문 사회":
+        //         resultCode += "H";
+        //         break;
+        //     case "자연":
+        //         resultCode += "N";
+        //         break;
+        //     case "공학":
+        //         resultCode += "E";
+        //         break;
+        //     case "예술":
+        //         resultCode += "A";
+        //         break;
+        //     case "체육":
+        //         resultCode += "P";
+        //         break;
+        // }
+
+        // // 전공 / 교양
+        // if (majorRatio >= 50) {
+        //     resultCode += "M";
+        // } else {
+        //     resultCode += "G";
+        // }
+
+        // // 공부 / 휴식
+        // if (studyTimeRatio >= 50) {
+        //     resultCode += "S";
+        // } else {
+        //     resultCode += "R";
+        // }
+
+        // // 야외 / 실내
+        // if (outsideRatio >= 50) {
+        //     resultCode += "O";
+        // } else {
+        //     resultCode += "I";
+        // }
+
+        // String resultName = "";
+
+        if (studyTime >= 10){
+            resultCode = "NMSI";
+            resultName = "침대 역학 아인슈타인";
+        }
+        else{
+            resultCode = "HGRO";
+            resultName = "궁궐 탈출 세종대왕";
         }
 
-        // 전공 / 교양
-        if (majorRatio >= 50) {
-            resultCode += "M";
-        } else {
-            resultCode += "G";
-        }
-
-        // 공부 / 휴식
-        if (studyTimeRatio >= 50) {
-            resultCode += "S";
-        } else {
-            resultCode += "R";
-        }
-
-        // 야외 / 실내
-        if (outsideRatio >= 50) {
-            resultCode += "O";
-        } else {
-            resultCode += "I";
-        }
-
-        String resultName = "";
-
-        switch (resultCode) {
-            case "HGRO":
-                resultName = "궁궐 탈출 세종대왕";
-                break;
-            case "NMSI":
-                resultName = "침대 역학 아인슈타인";
-                break;
-            case "EMSI":
-                resultName = "오늘도 또 유레카 에디슨";
-                break;
-            case "AMRO":
-                resultName = "별 보러 산 간 고흐";
-                break;
-            case "PGRO":
-                resultName = "스피닝 타는 엄복동";
-                break;
-        }
+        // switch (resultCode) {
+        //     case "HGRO":
+        //         resultName = "궁궐 탈출 세종대왕";
+        //         break;
+        //     case "NMSI":
+        //         resultName = "침대 역학 아인슈타인";
+        //         break;
+        //     case "EMSI":
+        //         resultName = "오늘도 또 유레카 에디슨";
+        //         break;
+        //     case "AMRO":
+        //         resultName = "별 보러 산 간 고흐";
+        //         break;
+        //     case "PGRO":
+        //         resultName = "스피닝 타는 엄복동";
+        //         break;
+        // }
 
         return Pair.of(resultCode, resultName);
     }
