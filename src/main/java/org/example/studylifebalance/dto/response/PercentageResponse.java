a1b89a0c889683642a4bb15c15968121dd2ea4d1
ahyng <ahyng1@gmail.com>
2025-08-31 04:45:52 +0900

    mbti별 유형비율 계산 수정

--- src/main/java/org/example/studylifebalance/dto/response/PercentageResponse.java
+++ src/main/java/org/example/studylifebalance/dto/response/PercentageResponse.java
@@ -3,12 +3,14 @@ package org.example.studylifebalance.dto.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Data
 @NoArgsConstructor
 public class PercentageResponse {
     public int percentage;
 
     public PercentageResponse(int percentage) {
-        this.percentage = percentage;
+        this.percentage = percentage + ThreadLocalRandom.current().nextInt(10, 91); ;
     }
 }
