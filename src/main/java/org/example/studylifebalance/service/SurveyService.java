ea32b479676868e771378fc10311f39913772390
ahyng <ahyng1@gmail.com>
2025-08-31 07:30:58 +0900

    Revert "outsideRatio 음수 문제 해"
    This reverts commit a5d00f7e6761769eca53338a1bc397d1d11e1d26.

--- src/main/java/org/example/studylifebalance/service/SurveyService.java
+++ src/main/java/org/example/studylifebalance/service/SurveyService.java
@@ -67,10 +67,11 @@ public class SurveyService {
         MBTI mbtiEnum = MBTI.valueOf(mbti.toUpperCase());
 
         // 점수 가져오기
-        double mbtiScore = mbtiEnum.getScore() + 1;
+        double mbtiScore = mbtiEnum.getScore();
         int outsideTimeScore = outsideTime ? 1 : 0;
-        double score = (mbtiScore + outsideTimeScore) / 2;
+        double score = (mbtiScore + outsideTimeScore) / 3;
         int roundedScore = (int) Math.round(score * 100);
+        if (roundedScore == 0) return 0;
         return roundedScore;
     }
 
