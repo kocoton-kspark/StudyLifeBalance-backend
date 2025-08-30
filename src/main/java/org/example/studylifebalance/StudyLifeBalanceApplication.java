a3d2351ce1ab4f47b09a35ebb239810bbb24c719
ahyng <ahyng1@gmail.com>
2025-08-30 20:38:30 +0900

    first commit

---
+++ src/main/java/org/example/studylifebalance/StudyLifeBalanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.studylifebalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLifeBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLifeBalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/studylifebalance/StudyLifeBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studylifebalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLifeBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
