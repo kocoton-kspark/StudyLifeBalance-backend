93c1d7df24f421909d98e18445550d7d620a4f3e
SeoyeonH <stellastar0921@gmail.com>
2025-08-31 01:54:03 +0900

    카테고리별/학교별 count 쿼리 및 서비스 로직 수정

--- src/main/java/org/example/studylifebalance/repository/UserRepository.java
+++ src/main/java/org/example/studylifebalance/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package org.example.studylifebalance.repository;
 
+import org.example.studylifebalance.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
+    int countByCategoryID(String categoryID);
+    int countByCategoryIDAndCollege(String categoryID, String college);
+    int countByCollege(String college);
+    int count();
 }
